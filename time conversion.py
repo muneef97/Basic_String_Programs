6c7b4c543b997c06c773e81902f22e60c9fccd37
Muneef <muneef97@gmail.com>
2020-05-21 17:06:41 +0530

    time convertion added

---
+++ time conversion.py
@@ -0,0 +1,12 @@
+def convert(str1):
+    if (str1[-2:]=="AM" and str1[:2]=="12"):
+        return ("00"+str1[2:-2])
+    elif (str1[-2:]=="AM"):
+        return (str1[:-2])
+    elif (str1[-2:]=="PM" and str1[:2]=="12"):
+        return ("00"+str1[2:-2])
+    else:
+        return (str(int(str1[:2])+12)+str1[2:-2])
+str1 = "01:15:45 PM"
+result = convert(str1)
+print(result)
