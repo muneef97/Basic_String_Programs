4e6202d98731a85cce4c31c57b1415ef9ff10218
Muneef <muneef97@gmail.com>
2020-05-21 15:35:13 +0530

    anagram added

---
+++ anagram.py
@@ -0,0 +1,25 @@
+def anagram(str1,str2):
+    if(len(str1)!=len(str2)):
+        return -1
+    else:
+        list1 = list(str1)
+        list2 = list(str2)
+        count = 0
+        for i in range(len(list1)):
+            j = 0
+            while (j < len(list2)):
+                if(list1[i]==list2[j]):
+                    list2.remove(list2[j])
+                j += 1
+        if(len(list2)== 0):
+            return 1
+        else:
+            return -1
+
+str1 = "neefum"
+str2 = "muneef"
+result = anagram(str1,str2)
+if(result == 1):
+    print("Anagram")
+else:
+    print("Not anagram")
